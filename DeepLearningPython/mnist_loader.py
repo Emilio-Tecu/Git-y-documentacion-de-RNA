4f30387ce53614bf9074a6dc1d7906bc6ee75928
Emilio Tecu <emiliotecu@gmail.com>
2025-09-03 23:28:26 -0600

    Documentación y limpieza de código (eliminados archivos innecesarios)

--- DeepLearningPython/mnist_loader.py
+++ DeepLearningPython/mnist_loader.py
@@ -2,38 +2,39 @@
 """
 mnist_loader
 ~~~~~~~~~~~~
-A library to load the MNIST image data.  For details of the data
-structures that are returned, see the doc strings for ``load_data``
-and ``load_data_wrapper``.  In practice, ``load_data_wrapper`` is the
-function usually called by our neural network code.
+Este archivo carga la base de datos MNIST de dígitos escritos a mano.
+Convierte las imágenes a vectores de 784 entradas y normaliza.
+Se divide en: 
+    - training_data (para entrenar la red)
+    - validation_data (para validar mientras se entrena)
+    - test_data (para evaluar precisión final)
 """
 
-#### Libraries
-# Standard library
+#### Librerías
 import pickle
 import gzip
-
-# Third-party libraries
 import numpy as np
 
 def load_data():
-    """Return the MNIST data as a tuple containing the training data,
-    the validation data, and the test data.
-    The ``training_data`` is returned as a tuple with two entries.
-    The first entry contains the actual training images.  This is a
-    numpy ndarray with 50,000 entries.  Each entry is, in turn, a
-    numpy ndarray with 784 values, representing the 28 * 28 = 784
-    pixels in a single MNIST image.
-    The second entry in the ``training_data`` tuple is a numpy ndarray
-    containing 50,000 entries.  Those entries are just the digit
-    values (0...9) for the corresponding images contained in the first
-    entry of the tuple.
-    The ``validation_data`` and ``test_data`` are similar, except
-    each contains only 10,000 images.
-    This is a nice data format, but for use in neural networks it's
-    helpful to modify the format of the ``training_data`` a little.
-    That's done in the wrapper function ``load_data_wrapper()``, see
-    below.
+    """
+    Devuelve los datos de MNIST como una tupla que contiene los
+    datos de entrenamiento, los datos de validación y los datos de
+    prueba. Los ``datos_de_entrenamiento`` se devuelven como una
+    tupla con dos entradas. La primera entrada contiene las 
+    imágenes de entrenamiento reales. Esta es un ndarray de numpy 
+    con 50,000 entradas. Cada entrada es, a su vez, un ndarray de 
+    numpy con 784 valores, que representan los 28 * 28 = 784 
+    píxeles en una sola imagen de MNIST. La segunda entrada de 
+    la tupla ``datos_de_entrenamiento`` es un ndarray de numpy 
+    que contiene 50,000 entradas. Esas entradas son solo los 
+    valores de dígito (0...9) para las imágenes correspondientes 
+    contenidas en la primera entrada de la tupla. 
+    Los ``datos_de_validación`` y ``datos_de_prueba`` son 
+    similares, excepto que cada uno contiene solo 10,000 imágenes. 
+    Este es un buen formato de datos, pero para su uso en 
+    redes neuronales es útil modificar un poco el formato de los 
+    ``datos_de_entrenamiento``. 
+    Esto se hace en la función envoltorio ``load_data_wrapper()``.
     """
     f = gzip.open('mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
@@ -41,23 +42,25 @@ def load_data():
     return (training_data, validation_data, test_data)
 
 def load_data_wrapper():
-    """Return a tuple containing ``(training_data, validation_data,
-    test_data)``. Based on ``load_data``, but the format is more
-    convenient for use in our implementation of neural networks.
-    In particular, ``training_data`` is a list containing 50,000
-    2-tuples ``(x, y)``.  ``x`` is a 784-dimensional numpy.ndarray
-    containing the input image.  ``y`` is a 10-dimensional
-    numpy.ndarray representing the unit vector corresponding to the
-    correct digit for ``x``.
-    ``validation_data`` and ``test_data`` are lists containing 10,000
-    2-tuples ``(x, y)``.  In each case, ``x`` is a 784-dimensional
-    numpy.ndarry containing the input image, and ``y`` is the
-    corresponding classification, i.e., the digit values (integers)
-    corresponding to ``x``.
-    Obviously, this means we're using slightly different formats for
-    the training data and the validation / test data.  These formats
-    turn out to be the most convenient for use in our neural network
-    code."""
+    """Devuelve una tupla que contiene ``(datos_de_entrenamiento, 
+    datos_de_validación, datos_de_prueba)``. Basado en 
+    ``cargar_datos``, pero el formato es más conveniente para su 
+    uso en nuestra implementación de redes neuronales. 
+    En particular, ``datos_de_entrenamiento`` es una lista que 
+    contiene 50,000 2-tuplas ``(x, y)``. ``x`` es un numpy.ndarray 
+    de 784 dimensiones que contiene la imagen de entrada. ``y`` 
+    es un numpy.ndarray de 10 dimensiones que representa el vector 
+    unitario correspondiente al dígito correcto para ``x``. 
+    ``datos_de_validación`` y ``datos_de_prueba`` son listas que 
+    contienen 10,000 2-tuplas ``(x, y)``. En cada caso, ``x`` es 
+    un numpy.ndarray de 784 dimensiones que contiene la imagen de 
+    entrada, y ``y`` es la clasificación correspondiente, es decir, 
+    los valores de dígitos (enteros) correspondientes a ``x``. 
+    Obviamente, esto significa que estamos utilizando formatos 
+    ligeramente diferentes para los datos de entrenamiento y 
+    los datos de validación / prueba. Estos formatos resultan 
+    ser los más convenientes para su uso en nuestro código de red 
+    neuronal."""
     tr_d, va_d, te_d = load_data()
     training_inputs = [np.reshape(x, (784, 1)) for x in tr_d[0]]
     training_results = [vectorized_result(y) for y in tr_d[1]]
@@ -69,10 +72,9 @@ def load_data_wrapper():
     return (training_data, validation_data, test_data)
 
 def vectorized_result(j):
-    """Return a 10-dimensional unit vector with a 1.0 in the jth
-    position and zeroes elsewhere.  This is used to convert a digit
-    (0...9) into a corresponding desired output from the neural
-    network."""
+    """Convierte una etiqueta entera (ej. 3) a un vector columna
+    con un 1 en la posición correspondiente.
+    Ejemplo: para '3' → [0,0,0,1,0,0,0,0,0,0]^T"""
     e = np.zeros((10, 1))
     e[j] = 1.0
     return e
--- DeepLearningPython/network.py
+++ DeepLearningPython/network.py
@@ -3,57 +3,67 @@
 """
 network.py
 ~~~~~~~~~~
-IT WORKS
-
-A module to implement the stochastic gradient descent learning
-algorithm for a feedforward neural network.  Gradients are calculated
-using backpropagation.  Note that I have focused on making the code
-simple, easily readable, and easily modifiable.  It is not optimized,
-and omits many desirable features.
+En este código se implementa una red neuronal para clasificar números hecha desde cero.
+Se utiliza 'class' que es un comando utilizado en programación orientado
+a objetos y define las propiedades y funciones de un objeto.
+La clase network contiene:
+    - Inicialización de pesos y bias.
+    - Propagación hacia adelante (feedfowrward).
+    - Entrenamiento empleando SGD.
+    - Y calculo de gradientes por backpropagation.
 """
 
-#### Libraries
-# Standard library
+#### Librerías
 import random
-
-# Third-party libraries
 import numpy as np
 
 class Network(object):
-
+# Usamos __init__ para configurar la red, definir capas, pesos, bias, etc.
     def __init__(self, sizes):
-        """The list ``sizes`` contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+        """
+        sizes: lista que contiene el número de neuronas por capa.
+        Ejemplo: [784, 30, 10] significa:
+        - 784 neuronas de entrada (imagen 28x28)
+        - 30 neuronas en la capa oculta
+        - 10 neuronas de salida (dígitos 0-9)
+
+        self.num_layers = número total de capas
+        self.sizes = lista de tamaños
+        self.biases = lista de vectores columna con los sesgos (bias)
+        self.weights = lista de matrices de pesos
+        """
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+        # Los bias y los weights inician con valores aleatorios con una distribución Gaussiana.
+        # No se considera la primera capa de bias porque son los datos de entrada.
+        # Se tiene un vector de biases y una matriz de weights.
 
     def feedforward(self, a):
-        """Return the output of the network if ``a`` is input."""
+        """
+        Calcula la salida de la red para una entrada 'a'.
+        Proceso:
+        1. Se multiplican pesos * entrada y se suman bias
+        2. Se aplica la función de activación sigmoide
+        3. Se repite para cada capa
+        Retorna: vector de activaciones de la última capa
+        """
         for b, w in zip(self.biases, self.weights):
             a = sigmoid(np.dot(w, a)+b)
         return a
+    # La función sigmoide se usa para tener continuidad en la transición de 0 a 1.
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
             test_data=None):
-        """Train the neural network using mini-batch stochastic
-        gradient descent.  The ``training_data`` is a list of tuples
-        ``(x, y)`` representing the training inputs and the desired
-        outputs.  The other non-optional parameters are
-        self-explanatory.  If ``test_data`` is provided then the
-        network will be evaluated against the test data after each
-        epoch, and partial progress printed out.  This is useful for
-        tracking progress, but slows things down substantially."""
+        """Entrena la red usando el algoritmo de Descenso de Gradiente Estocástico (SGD).
+        Parámetros:
+        -training_data: datos de entrenamiento.
+        -epochs: número de épocas (vueltas completas a los datos).
+        -mini_batch_size: tamaño de cada sub-lote de entrenamiento.
+        -eta: tasa de aprendizaje.
+        -test_data: datos de prueba opcionales para monitorear precisión."""
 
         training_data = list(training_data)
         n = len(training_data)
@@ -75,10 +85,9 @@ class Network(object):
                 print("Epoch {} complete".format(j))
 
     def update_mini_batch(self, mini_batch, eta):
-        """Update the network's weights and biases by applying
-        gradient descent using backpropagation to a single mini batch.
-        The ``mini_batch`` is a list of tuples ``(x, y)``, and ``eta``
-        is the learning rate."""
+        """Va actualizando los pesos y biases de la red usando backpropagation
+        y recibe un mini_batch de datos, para ajustar los parámetros
+        en dirección opuesta al gradiente."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
         for x, y in mini_batch:
@@ -91,16 +100,18 @@ class Network(object):
                        for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
-        """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+        """Aplica el algoritmo de backpropagation.
+        Retorna:
+        - gradientes de pesos
+        - gradientes de biases
+        Sirve para calcular cómo ajustar cada parámetro de la red
+        para minimizar el error de salida."""
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        # feedforward
+        # Este es el feedforward.
         activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activations = [x] # Lista para guardar todas las activaciones, capa por capa.
+        zs = [] # Lista para guardar todos los vectores z, capa por capa.
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
@@ -111,12 +122,6 @@ class Network(object):
             sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        # Note that the variable l in the loop below is used a little
-        # differently to the notation in Chapter 2 of the book.  Here,
-        # l = 1 means the last layer of neurons, l = 2 is the
-        # second-last layer, and so on.  It's a renumbering of the
-        # scheme in the book, used here to take advantage of the fact
-        # that Python can use negative indices in lists.
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
@@ -126,24 +131,23 @@ class Network(object):
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
+        """Evalúa la presición de la red en datos de prueba.
+        Regresa el número de aciertos hechos por la red."""
         test_results = [(np.argmax(self.feedforward(x)), y)
                         for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        r"""Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
+        r"""Regresa el vector de derivadas parciales de la función de costo."""
         return (output_activations-y)
 
-#### Miscellaneous functions
+#### Funciones auxiliares.
 def sigmoid(z):
-    """The sigmoid function."""
+    """Función de activación sigmoide."""
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    """Derivative of the sigmoid function."""
+    """Derivada de la función sigmoide"""
     return sigmoid(z)*(1-sigmoid(z))
+# Cada que utilicemos una función de activación, necesitamos la 
+# función misma y su derivada.
